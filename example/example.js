c12e68950a071b62eeca67717d4fcb1aaf07254b
邵玉棋 <shaoyuqi@hujiang.com>
2021-11-18 22:08:11 +0800

    update

--- example/example.js
+++ example/example.js
@@ -6,7 +6,6 @@ import createThunkMiddleware from './createThunkMiddleware.js'
 // 默认第一次会执行一次默认的dispatch，action的值是随机生成的字符串
 // 因此会走reducer默认action分支，生成默认的state数据
 let store = createStore(reducer,applyMiddleware(createThunkMiddleware()))
-// applyMiddleware(createThunkMiddleware()) 执行后返回的是一个函数
 
 store.subscribe(() => console.log(store.getState()))
 
